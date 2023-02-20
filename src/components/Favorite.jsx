1fac455ee786ca271ef0adf3adbd15229fec02b5
gyulaidavid <gyulaidavid@gmail.com>
2023-02-20 18:01:52 +0100

    cleaning in favorite.jsx

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import PushPinOutlinedIcon from "@mui/icons-material/PushPinOutlined";
 import PushPinIcon from "@mui/icons-material/PushPin";
 import { Button } from "@mui/material";
-import { addCountry, deleteCountry } from "../DataManager";
-import { async } from "@firebase/util";
 
 function Favorite() {
   const [isFavorite, setIsFavorite] = useState(false);
