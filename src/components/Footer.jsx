83c73ee0d2d4cf2f0fabbda8a30ad3834801985d
gyulaidavid <gyulaidavid@gmail.com>
2023-02-21 00:22:09 +0100

    Footer design ready, link needed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,76 @@
-import React from 'react'
-import Typography from "@mui/material/Typography";
+import React from "react";
+import { Typography, Paper, Box, Container, Grid, Item } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { textAlign } from "@mui/system";
 
 function Footer() {
   return (
-    <Typography variant='h4'>Footer</Typography>
-  )
+    <Paper
+      sx={{
+        marginTop: "calc(10% + 60px)",
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+      
+      }}
+      component='footer'
+      square
+      variant='outlined'
+    >
+      <Container maxWidth='lg'>
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            my: 2,
+            
+          }}
+        ></Box>
+        <Box sx={{ mb: 2, flexGrow: 1, textAlign: "center" }}>
+          <Grid
+            alignItems='center'
+            justifyContent='center'
+            container
+            spacing={{ xs: 1, md: 1 }}
+            columns={{ xs: 1, sm: 1, md: 12 }}
+          >
+            <Grid item xs={1}>
+              <Typography>
+                <FacebookIcon />
+              </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <Typography>
+                <GitHubIcon />
+              </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <Typography>
+                <LinkedInIcon />
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+            mb: 2,
+          }}
+        >
+          <Typography variant='caption' color='initial'>
+            Copyright ©2023. All Right Reserved.
+          </Typography>
+        </Box>
+      </Container>
+    </Paper>
+  );
 }
 
-export default Footer
+export default Footer;
