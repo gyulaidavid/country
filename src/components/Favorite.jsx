292d1431d71a31d7309e9ccf79ca4d61a14bf74a
gyulaidavid <gyulaidavid@gmail.com>
2023-02-20 18:00:41 +0100

    works

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -5,29 +5,23 @@ import { Button } from "@mui/material";
 import { addCountry, deleteCountry } from "../DataManager";
 import { async } from "@firebase/util";
 
-function Favorite({ country }) {
+function Favorite() {
   const [isFavorite, setIsFavorite] = useState(false);
-  const [countryName, setCountryName] = useState(country.name.official);
-  const [countryCapital, setCountryCapital] = useState(country.capital);
+
 
   return (
     <div>
       <Button
         sx={{ color: "#A7C7E7" }}
-        onClick={async () => {
-          await addCountry(countryName, countryCapital);
+        onClick={ () => {
+          
           setIsFavorite((oldValue) => !oldValue);
         }}
       >
         {isFavorite ? <PushPinIcon /> : <PushPinOutlinedIcon />}
       </Button>
 
-      <Button
-      onClick={async () => {
-        await deleteCountry()
-      }}
-      
-      variant="outline">Remove from favorite</Button>
+     
     </div>
   );
 }
