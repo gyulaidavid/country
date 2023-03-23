4a97a67a497d17b48c94e502695f85c6be79776d
gyulaidavid <gyulaidavid@gmail.com>
2023-03-23 10:28:37 +0100

    Countrydetails card done

--- src/pages/Countrydetails.jsx
+++ src/pages/Countrydetails.jsx
@@ -8,8 +8,11 @@ import {
   Modal,
   Tooltip,
   GlobalStyles,
-  Card, CardContent, CardMedia, CardActionArea, CardActions 
-
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+  CardActions,
 } from "@mui/material";
 
 const style = {
@@ -44,69 +47,91 @@ function Countrydetails({ countries }) {
           body: { backgroundColor: "#A7C7E7" },
         }}
       />
-      
-        <Box sx={{ p: 1, display: "flex", color: "white" }}>
-          {countries
-            .filter((filterCountry) => filterCountry.name.official === id)
-            .map((filterCountry, i) => (
-              <div key={i}>
-               
+
+      <Box
+        sx={{
+          p: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {countries
+          .filter((filterCountry) => filterCountry.name.official === id)
+          .map((filterCountry, i) => (
+            <div key={i}>
+              <Card sx={{ maxWidth: 500, boxShadow: 10, p: 2, backgroundColor: "#78A2CC"}}>
+                <CardMedia
+                  component='img'
+                  height='100%'
+                  image={filterCountry.flags.png}
+                  alt={filterCountry.name.official}
                   
-                    <img
-                      src={filterCountry.flags.png}
-                      alt='{filterCountry.name.official}'
-                    />
-                  
+                />
 
-                  <Typography variant='h3' sx={{ pb: 2,  display: 'flex', flexDirection: 'row'}}>
-                    {filterCountry.name.official} <Favorite />
-                  </Typography>
-                
-                <Typography variant='h4' sx={{ pb: 2 }}>
-                  Capital: {filterCountry.capital}
-                </Typography>
-
-                {/* Coat-of-arms Start */}
-
-                <Box>
-                  {/* //Bouvet Island  */}
-                  {filterCountry.coatOfArms.hasOwnProperty("png") ? (
-                    <Button onClick={handleOpen}>
-                      <Tooltip title='Click to enlarge image' placement='right'>
-                        <img
-                          height={200}
-                          src={filterCountry.coatOfArms.png}
-                          alt='Click to enlarge image'
-                        />
-                      </Tooltip>
-                    </Button>
-                  ) : (
-                    "Nincs címer"
-                  )}
-
-                  <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby='modal-modal-title'
-                    aria-describedby='modal-modal-description'
+                <CardContent
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: 'white'
+                  }}
+                >
+                  <Typography
+                    variant='h3'
+                    sx={{ pb: 2 }}
                   >
-                    <Box sx={style}>
-                      <Typography
-                        id='modal-modal-title'
-                        variant='h6'
-                        component='h2'
-                      >
-                        <img
-                          height={600}
-                          src={filterCountry.coatOfArms.png}
-                          alt='coat-of-arms'
-                        />
-                      </Typography>
-                    </Box>
-                  </Modal>
-                </Box>
+                    {filterCountry.name.official} 
+                  </Typography>
 
-                <Box>
+                  <Typography variant='h4' sx={{ pb: 2 }}>
+                    Capital: {filterCountry.capital}
+                  </Typography>
+
+                  {/* Coat-of-arms Start */}
+
+                  <Box>
+                    
+                    {filterCountry.coatOfArms.hasOwnProperty("png") ? (
+                      <Button onClick={handleOpen}>
+                        <Tooltip
+                          title='Click to enlarge image'
+                          placement='right'
+                        >
+                          <img
+                            height={200}
+                            src={filterCountry.coatOfArms.png}
+                            alt='Click to enlarge image'
+                          />
+                        </Tooltip>
+                      </Button>
+                    ) : (
+                      "Coat of arms is not available from the API."
+                    )}
+
+                    <Modal
+                      open={open}
+                      onClose={handleClose}
+                      aria-labelledby='modal-modal-title'
+                      aria-describedby='modal-modal-description'
+                    >
+                      <Box sx={style}>
+                        <Typography
+                          id='modal-modal-title'
+                          variant='h6'
+                          component='h2'
+                        >
+                          <img
+                            height={600}
+                            src={filterCountry.coatOfArms.png}
+                            alt='coat-of-arms'
+                          />
+                        </Typography>
+                      </Box>
+                    </Modal>
+                  </Box>
+
                   <Typography sx={{ my: 1 }} variant='h6'>
                     Continent: {filterCountry.continents}
                   </Typography>
@@ -117,11 +142,15 @@ function Countrydetails({ countries }) {
                   <Typography sx={{ mb: 1 }} variant='h6'>
                     Population: {formatNumber(filterCountry.population)}
                   </Typography>
-                </Box>
-              </div>
-            ))}
-        </Box>
-     
+                  <CardActions>
+                <Favorite />
+                </CardActions>
+                </CardContent>
+                
+              </Card>
+            </div>
+          ))}
+      </Box>
     </>
   );
 }
